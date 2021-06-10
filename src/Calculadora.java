a3299ef4d9550d038b271ab6faf19ec0a8c1f647
Thiago Stteffen <thiagokalu@gmail.com>
2021-06-10 19:56:30 -0300

    aula da calculadora com as 4 operacoes

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,14 +6,23 @@ public class Calculadora {
 		Scanner leitor = new Scanner(System.in);
 		double valor1;
 		double valor2;	
-		double soma;
+		double soma, divisao, multiplicacao, subtracao;
 		System.out.println("Por favor, digite um valor:");
 		valor1 = leitor.nextDouble();
 		System.out.println("Por favor, digite mais um valor:");
 		valor2 = leitor.nextDouble();
 		
 		soma = valor1 + valor2;
+		subtracao = valor1 - valor2;
+		multiplicacao = valor1 * valor2;
+		divisao = valor1 / valor2;
+		
 		System.out.println("A soma dos dois valores é " + soma);
+		System.out.println("A subtração dos dois valores é " + subtracao);
+		System.out.println("A multiplicacao dos dois valores é " + multiplicacao);
+		System.out.println("divisao dos dois valores é " + divisao);
+		leitor.close();
+		
 	}
 
 }
